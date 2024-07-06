26dcd70071d7ec34eed874ac99b7631d6eef8ea1
louispaulet <louispaulet13@gmail.com>
2024-07-06 02:08:53 +0200

    switch to cloud function API

--- timeline_frontend/src/config.js
+++ timeline_frontend/src/config.js
@@ -1,7 +1,8 @@
 // config.js
 
 const config = {
-  apiBaseUrl: 'http://localhost:8000' // Change this to your cloud API URL before deploying
+  //apiBaseUrl: 'http://localhost:8000' // local test url
+  apiBaseUrl: 'https://timeline-backend-itw4ovf5zq-ew.a.run.app/' //cloud api url
 };
 
 export default config;
