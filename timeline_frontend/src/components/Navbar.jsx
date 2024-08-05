50492cf0b611ff8605f77362daa2f0518e6c8aa4
louispaulet <louispaulet13@gmail.com>
2024-08-05 10:45:18 +0200

    make logo and website name redirect to home in navbar

--- timeline_frontend/src/components/Navbar.jsx
+++ timeline_frontend/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import timelineFavicon from './../../public/timeline_favicon.webp'; // Adjust the path based on your project structure
+import timelineFavicon from './../../public/timeline_favicon.webp';
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <img src={timelineFavicon} alt="Website Icon" className="w-8 h-8 mr-2" />
-          <div className="text-white text-xl">Timeline App</div>
+          <Link to="/" className="flex items-center">
+            <img src={timelineFavicon} alt="Website Icon" className="w-8 h-8 mr-2" />
+            <div className="text-white text-xl">Timeline App</div>
+          </Link>
         </div>
         <div className="flex space-x-4">
           <Link to="/" className="text-white">Home</Link>
